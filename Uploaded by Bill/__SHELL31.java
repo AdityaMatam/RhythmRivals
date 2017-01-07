684e548f7cb1b9a55e627396cbc9c8f376dc6fc4
SCPD101 <billhuang2009@hotmail.com>
2017-01-07 14:43:44 -0500

    Add files via upload

---
+++ Uploaded by Bill/__SHELL31.java
@@ -0,0 +1,11 @@
+
+public class __SHELL31 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("E:\\Others\\Placeholder");
+final Scoreboard scoreboard = (Scoreboard)__bluej_runtime_scope.get("scoreboard");
+
+
+int __bluej_param0 = 960;
+scoreboard.addScore(__bluej_param0);
+
+}}
