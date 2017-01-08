2e4f2ba2b7e0dbadd90296058431a80170a69033
Alex Wong <alexw.wong@mail.utoronto.ca>
2017-01-08 08:54:00 -0500

    Added title

---
+++ RhythmRivals/Title.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.*;
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends Actor
+{
+    public Title(){
+        setImage (new GreenfootImage ("Rhythm Rivals", 40, Color.BLACK, new Color(0,0,0,0)));
+    }
+    /**
+     * Act - do whatever the Title wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
